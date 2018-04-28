3c9f6de8ac3bdbcaba110f29d549513922255fad
Melissa Franklin <trebleclef777@gmail.com>
2018-04-28 18:35:02 -0500

    commit

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 
 
+
 app = Flask(__name__)
 app.config['DEBUG'] = True
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://blogz:buildtheblogz@localhost:8889/blogz'
